cc2bea1c712300c2b06fa84dba1043aec770ceee
Oliver Meschkov <omeschkov@mailbox.org>
2022-03-25 20:31:51 +0100

    WiP

--- cmd/main.go
+++ cmd/main.go
@@ -15,26 +15,41 @@ type PageData struct {
 }
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		w.Header().Set("Content-Type", "text/html")
-		tmp, err := template.ParseFiles(fmt.Sprintf("%s/root.gohtml", TemplatePathPrefix))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		err = tmp.Execute(w, PageData{Title: "One Record Server"})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	w.Header().Set("Content-Type", "text/html")
+	tmp, err := template.ParseFiles(fmt.Sprintf("%s/root.html", TemplatePathPrefix))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmp.Execute(w, PageData{Title: "One Record Server"})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
+func customAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RequestURI)
+		name := r.Header.Get(AuthHeaderNameKey)
+		token := r.Header.Get(AuthHeaderTokenKey)
+		if name == AuthHeaderNameValue && token == AuthHeaderTokenValue {
+			// Call the next handler, which can be another middleware in the chain, or the final handler.
+			next.ServeHTTP(w, r)
+		} else {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+	})
+}
+
 func main() {
-	log.Println("Server will start at")
+	log.Println("Server will start at :8000")
 	fs := http.FileServer(http.Dir("./web/public/static/"))
-	route := mux.NewRouter()
-	route.PathPrefix("/static").Handler(http.StripPrefix("/static", fs))
-	route.HandleFunc("/", RootHandler)
-	log.Fatal(http.ListenAndServe(":8000", route))
+	router := mux.NewRouter()
+	router.PathPrefix("/static").Handler(http.StripPrefix("/static", fs))
+	router.HandleFunc("/", RootHandler).Methods(http.MethodGet, http.MethodOptions)
+	router.Use(customAuthMiddleware)
+	router.Use(mux.CORSMethodMiddleware(router))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
