842efe9192ff2499ac1a9ce0342e70811ee4ad55
Oliver Meschkov <omeschkov@mailbox.org>
2022-03-25 21:37:19 +0100

    Added service package to load env variables to a Service struct

--- service/config.go
+++ service/config.go
@@ -1,4 +1,16 @@
 package service
 
 type Service struct {
+	Auth struct {
+		NameValue  string `env:"AUTH_NAME_VALUE"`
+		NameKey    string `env:"AUTH_NAME_KEY"`
+		TokenKey   string `env:"AUTH_TOKEN_KEY"`
+		TokenValue string `env:"AUTH_TOKEN_VALUE"`
+	}
+	Path struct {
+		Template string `env:"PATH_TEMPLATE"`
+		Static   string `env:"PATH_STATIC"`
+	}
+	ServerRole string `env:"SRV_ROLE"`
+	ProjectId  string `env:"PROJECT_ID"`
 }
