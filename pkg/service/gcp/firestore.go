b6e90a2cf6fc39a25316006845be70ee331ed2f7
Oliver Meschkov <omeschkov@mailbox.org>
2022-04-09 12:04:32 +0200

    Added firestore service

---
+++ pkg/service/gcp/firestore.go
@@ -0,0 +1,47 @@
+package gcp
+
+import (
+	"cloud.google.com/go/firestore"
+	"context"
+	"github.com/chi-deutschland/one-record-server/pkg/model"
+	"github.com/chi-deutschland/one-record-server/pkg/service"
+	"google.golang.org/api/iterator"
+)
+
+type FirestoreService struct {
+}
+
+func (f *FirestoreService) GetCompanies(projectID string) ([]model.Company, error) {
+	var companies []model.Company
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		return companies, err
+	}
+	iter := client.Collection("companies").Documents(ctx)
+	for {
+		var company model.Company
+
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return companies, err
+		}
+
+		if err := doc.DataTo(&company); err != nil {
+			return companies, err
+		}
+
+		companies = append(companies, company)
+	}
+
+	return companies, nil
+}
+
+func NewFirestoreService() (*FirestoreService, error) {
+	var f FirestoreService
+	return &f, nil
+}
+
+var _ service.DBService = (*FirestoreService)(nil)
